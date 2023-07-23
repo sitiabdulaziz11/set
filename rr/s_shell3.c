e11e95e00d154083b05a2c4324c15d2ee49b3383
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-23 22:39:38 +0300

    folder

---
+++ rr/s_shell3.c
@@ -0,0 +1,264 @@
+#include "shell.h"
+/**
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+/**
+ * puts_err - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void puts_err(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		putchar_err(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[BUFFER_SIZE_WRITE];
+
+	if (c == NEG_ONE || i >= BUFFER_SIZE_WRITE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != NEG_ONE)
+		buf[i++] = c;
+	return (1);
+}
+/**
+ * putchar_err - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On failure, -1 is returned
+ */
+int putchar_err(char c)
+{
+	static int i;
+	static char buf[BUFFER_SIZE_WRITE];
+
+	if (c == NEG_ONE || i >= BUFFER_SIZE_WRITE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != NEG_ONE)
+		buf[i++] = c;
+	return (1);
+}
+/**
+ * print_error - prints an error message
+ * @info: the parameter & return info struct
+ * @estr: string containing specified error type
+ * Return: 0 if no numbers in string,
+ * converted integer else -1
+ */
+void print_error(info_s *info, char *estr)
+{
+	puts_err(info->prog_name);
+	puts_err(": ");
+	print_dec(info->lines, STDERR_FILENO);
+	puts_err(": ");
+	puts_err(info->argv[0]);
+	puts_err(": ");
+	puts_err(estr);
+}
+
+/**
+ * check_chain - checks we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ * @i: starting position in buf
+ * @len: length of buf
+ *
+ * Return: Void
+ */
+void check_chain(info_s *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t j = *p;
+
+	if (info->sep_buff_kind == AND_FLAG)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	if (info->sep_buff_kind == OR_FLAG)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	*p = j;
+}
+/**
+ * is_chain - test if current char in buffer is a chain delimeter
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ *
+ * Return: 1 if chain delimeter else 0
+ */
+bool is_chain(info_s *info, char *buf, size_t *p)
+{
+	size_t j = *p;
+
+	if (buf[j] == '|' && buf[j + 1] == '|')
+	{
+		buf[j] = 0;
+		j++;
+		info->sep_buff_kind = OR_FLAG;
+	}
+	else if (buf[j] == '&' && buf[j + 1] == '&')
+	{
+		buf[j] = 0;
+		j++;
+		info->sep_buff_kind = AND_FLAG;
+	}
+	else if (buf[j] == ';')
+	{
+		buf[j] = 0;
+		info->sep_buff_kind = CHAIN_FLAG;
+	}
+	else
+		return (false);
+	*p = j;
+	return (true);
+}
+/**
+ * print_dec - function prints a decimal (integer) number (base 10)
+ * @input: the input
+ * @fd: the filedescriptor to write to
+ *
+ * Return: number of characters printed
+ */
+int print_dec(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = putchar_err;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+	return (count);
+}
+
+/**
+ * list_to_vector - returns an array of strings of the list->str
+ * @head: pointer to first node
+ *
+ * Return: Array of strings.
+ */
+
+char **list_to_vector(list_s *head)
+{
+	list_s *node = head;
+	size_t i = _listlen(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+size_t _listlen(const list_s *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	 while (src[i])
+	 {
+		 dest[i] = src[i];
+		 i++;
+	 }
+	 dest[i] = 0;
+	 return (dest);
+}
---
+++ rr/s_shell5.c
@@ -0,0 +1,220 @@
+#include "shell.h"
+/**
+ * check_file_in_path - finds this cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *check_file_in_path(info_s *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_executable(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_executable(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * input_buf - buffers chained commands
+ * @info: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
+ *
+ * Return: bytes read
+ */
+ssize_t input_buf(info_s *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len) /* if nothing left in the buffer, fill it */
+	{
+		/*bfree((void **)info->sep_buff);*/
+		free(*buf);
+		*buf = NULL;
+		
+/*#if USE_GETLINE*/
+		r = getline(buf, &len_p, stdin);
+/*#else
+		r = _getline(info, buf, &len_p);
+#endif*/
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->lc_flag = 1;
+			/* if (_strchr(*buf, ';')) is this a command chain? */
+			{
+				*len = r;
+				info->sep_buff = buf;
+			}
+		}
+	}
+	return (r);
+}
+/**
+ * print_list_str - prints only the str element of a list_s linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list_str(const list_s *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+/**
+ * free_vector - frees memory allocated to a 2D character array
+ * @vec: Vector to be freed.
+ *
+ * Return: Nothing.
+ */
+
+void free_vector(char **vec)
+{
+	char **ptr = vec;
+
+	if (!vec)
+		return;
+	while (*vec)
+		free(*vec++);
+
+	free(ptr);
+}/**
+ * free_list - frees all nodes of a list
+ * @head_ptr: address of pointer to head node
+ *
+ * Return: void
+ */
+void free_list(list_s **head_ptr)
+{
+	list_s *node, *next_node, *head;
+
+	if (!head_ptr || !*head_ptr)
+		return;
+	head = *head_ptr;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+	*head_ptr = NULL;
+}
+/**
+ * gather_env - populates env linked list
+ * @info: contains simulated arguments for a function pointer,
+ * allowing for a consistent function prototype
+ * Return: Always 0
+ */
+ 
+int gather_env(info_s *info)
+{
+	list_s *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+/**
+ * add_node_end - adds a node to the end of the list
+ * @head: address of pointer to head node
+ * @str: str field of node
+ * @num: node index used by history
+ *
+ * Return: size of list
+ */
+list_s *add_node_end(list_s **head, const char *str, int num)
+{
+	list_s *new_node, *node;
+	
+	if (!head)
+		return (NULL);
+	
+	node = *head;
+	new_node = malloc(sizeof(list_s));
+	if (!new_node)
+		return (NULL);
+	
+	_memset((void *)new_node, 0, sizeof(list_s));
+	new_node->num = num;
+	if (str)
+	{
+		new_node->str = _strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the pointer to the memory area
+ * @b: the byte to fill *s with
+ * @n: the amount of bytes to be filled
+ *
+ * Return: s- a pointer to the memory area of s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
